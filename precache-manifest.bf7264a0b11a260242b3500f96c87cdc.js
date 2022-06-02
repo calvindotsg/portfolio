c29d4da347e6b390543538fc77ba83bec14962d6
Calvin <68703834+calvin-sg@users.noreply.github.com>
2022-06-02 22:13:57 +0800

    Updates

---
+++ precache-manifest.bf7264a0b11a260242b3500f96c87cdc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85749f7540758d18ec8e97668fa8d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e6c303aacc06c60341c",
+    "url": "/static/css/main.1935c25c.chunk.css"
+  },
+  {
+    "revision": "9e4f6c43e2b0e1a45dc4",
+    "url": "/static/js/2.610d058f.chunk.js"
+  },
+  {
+    "revision": "84726a1e569a641a975d89466cc686da",
+    "url": "/static/js/2.610d058f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6c303aacc06c60341c",
+    "url": "/static/js/main.11919631.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "bd8dfc3315c8c59af7c9e483d0a3842b",
+    "url": "/static/media/Resume_Calvin.bd8dfc33.pdf"
+  },
+  {
+    "revision": "8e5ec63057ac4a4ddd3e65360e24768b",
+    "url": "/static/media/hero.8e5ec630.mp4"
+  },
+  {
+    "revision": "0137f090a52e466ecf767b21da279148",
+    "url": "/static/media/me.0137f090.jpg"
+  }
+]);
