9e9ccc045c8ac66354a979b35b204f8ba3a082e3
Calvin <68703834+calvin-sg@users.noreply.github.com>
2022-06-04 17:21:25 +0800

    Fixes to SlideUpWhenVisible and package.json
    1. Lowered SlideUpWhenVisible threshold to reduce scroll threshold before sections are visible
    2. Fixed name field in package.json

--- src/hooks/SlideUpWhenVisible.js
+++ src/hooks/SlideUpWhenVisible.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { motion, useAnimation } from 'framer-motion'
-import { useEffect } from 'react'
-import { useInView } from 'react-intersection-observer'
+import {motion, useAnimation} from 'framer-motion'
+import {useEffect} from 'react'
+import {useInView} from 'react-intersection-observer'
 
-export default function SlideUpWhenVisible({ children, threshold }) {
+export default function SlideUpWhenVisible({children, threshold}) {
     const controls = useAnimation()
-    const [ref, inView] = useInView({ threshold: threshold ? threshold : 0.35 })
+    const [ref, inView] = useInView({
+        threshold: threshold ? threshold : 0.1
+    })
 
     useEffect(() => {
         if (inView) {
@@ -17,10 +19,10 @@ export default function SlideUpWhenVisible({ children, threshold }) {
             ref={ref}
             animate={controls}
             initial='hidden'
-            transition={{ duration: 0.4 }}
+            transition={{duration: 0.4}}
             variants={{
-                visible: { opacity: 1, y: 0 },
-                hidden: { opacity: 0, y: 20 },
+                visible: {opacity: 1, y: 0},
+                hidden: {opacity: 0, y: 20},
             }}
         >
             {children}
